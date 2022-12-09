9d7110d1107df61581aa610455e8313e3238089f
MavichFN <maksbaluk5575@gmail.com>
2022-12-09 18:54:57 +0200

    Characteristic Animals

--- main.py
+++ main.py
@@ -7,5 +7,13 @@ class Pet:
             self.Cat = Cat
             self.Parrot = Parrot
         def add_animals(self, Pet):
-            self.animals.append(Pet)
+            self.Animals.append(Pet)
+
+Characteristic = ("Animals", "Name")
+# Characteristic.add_animals(Pet("Graf"))
+# Characteristic.add_animals(Pet("Max"))
+# Characteristic.add_animals(Pet("Blue"))
+# Characteristic.add_animals(Pet("Lucky"))
+# Characteristic.add_animals(Pet("Awful"))
+Characteristic.print_add_animals()
 
