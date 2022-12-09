8b7e0c60a15c4727bdda23482728b16725fde744
MavichFN <maksbaluk5575@gmail.com>
2022-12-09 18:44:58 +0200

    class Animals

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 class Pet:
     def __init__(self, name):
         self.name = name
+    class Animals:
+        def __init__(self, Dog, Cat, Parrot):
+            self.Dog = Dog
+            self.Cat = Cat
+            self.Parrot = Parrot
